c3b89ca3a75b7df901da75a042c13dca6e9bb19f
Theo <theophong@outlook.fr>
2024-12-19 18:54:40 +0100

    TP4 Coverage PokemonMetadataProviderTest 3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -55,4 +55,11 @@ public class IPokemonMetadataProviderTest {
         assertEquals("Mew", lastMetadata.getName()); // Exemple
     }
 
+    @Test
+    public void testPokemonsMetadataSize() {
+        assertNotNull(metadataProvider);
+        assertEquals(151, metadataProvider.pokemonsMetadata.size());
+    }
+
+
 }
