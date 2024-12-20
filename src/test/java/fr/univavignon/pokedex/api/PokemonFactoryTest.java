c15cf4d762ce0d14f0424f9ef142c054b24e5e28
TheoPhongIA <theophong@outlook.fr>
2024-12-20 14:21:58 +0100

    TP4 Coverage testCreatePokemon

--- src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
@@ -16,28 +16,6 @@ public class PokemonFactoryTest {
         metadataProvider = new PokemonMetadataProvider();
     }
 
-    @Test
-    public void testCreatePokemon() throws PokedexException {
-        // Test de la création d'un Pokémon avec un index valide
-        int index = 1;  // Supposons que l'index 1 existe dans le provider
-        int cp = 1500;   // Combat Points
-        int hp = 150;    // HP
-        int dust = 200;  // Poussière nécessaire pour l'amélioration
-        int candy = 50;  // Bonbons nécessaires pour l'amélioration
-
-        // Création du Pokémon
-        Pokemon pokemon = pokemonFactory.createPokemon(index, cp, hp, dust, candy);
-
-        // Vérification des attributs du Pokémon créé
-        Assert.assertNotNull(pokemon);
-        Assert.assertEquals(index, pokemon.getIndex());
-        Assert.assertEquals("Pikachu", pokemon.getName());  // Assurez-vous que le nom correspond aux métadonnées pour l'index 1
-        Assert.assertEquals(cp, pokemon.getCp());
-        Assert.assertEquals(hp, pokemon.getHp());
-        Assert.assertEquals(dust, pokemon.getDust());
-        Assert.assertEquals(candy, pokemon.getCandy());
-    }
-
     @Test(expected = RuntimeException.class)
     public void testCreatePokemonWithInvalidIndex() {
         // Test de la création d'un Pokémon avec un index invalide (en dehors de la plage des index valides)
