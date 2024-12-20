5cc845a14e48a2f2eafa6cab6c9b51b527ce4fd6
TheoPhongIA <theophong@outlook.fr>
2024-12-20 14:42:47 +0100

    TP4 Coverage Test sur PokemonMetadataProviderTest 3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -2,23 +2,33 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class IPokemonMetadataProviderTest {
-    private IPokemonMetadataProvider metadataProvider;
+
+    @Mock
+    private IPokemonMetadataProvider metadataProvider; // Le mock de l'interface
+
     @Before
     public void setUp() throws PokedexException {
-        // Création du mock de l'interface
-        metadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+        // Initialise les mocks
+        MockitoAnnotations.initMocks(this);
+
         // Simuler une réponse quand getPokemonMetadata est appelée
         PokemonMetadata bulbasaurMetadata = new PokemonMetadata(1, "Bulbasaur", 126, 126, 90);
-        Mockito.when(metadataProvider.getPokemonMetadata(1)).thenReturn(bulbasaurMetadata);
+        when(metadataProvider.getPokemonMetadata(1)).thenReturn(bulbasaurMetadata);
     }
+
     @Test
     public void testGetPokemonMetadata() throws PokedexException {
         // Appeler la méthode sur le mock
         PokemonMetadata metadata = metadataProvider.getPokemonMetadata(1);
+
         // Vérification des résultats attendus
         assertEquals("Bulbasaur", metadata.getName());
         assertEquals(126, metadata.getAttack());
