9b969571d945502f604c9a86eff10a1e3f7c8eda
TheoPhongIA <theophong@outlook.fr>
2024-12-20 14:47:49 +0100

    TP4 Coverage Test sur PokemonMetadataProviderTest 4

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -2,33 +2,23 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
 
 public class IPokemonMetadataProviderTest {
-
-    @Mock
-    private IPokemonMetadataProvider metadataProvider; // Le mock de l'interface
-
+    private IPokemonMetadataProvider metadataProvider;
     @Before
     public void setUp() throws PokedexException {
-        // Initialise les mocks
-        MockitoAnnotations.initMocks(this);
-
+        // Création du mock de l'interface
+        metadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
         // Simuler une réponse quand getPokemonMetadata est appelée
         PokemonMetadata bulbasaurMetadata = new PokemonMetadata(1, "Bulbasaur", 126, 126, 90);
-        when(metadataProvider.getPokemonMetadata(1)).thenReturn(bulbasaurMetadata);
+        Mockito.when(metadataProvider.getPokemonMetadata(1)).thenReturn(bulbasaurMetadata);
     }
-
     @Test
     public void testGetPokemonMetadata() throws PokedexException {
         // Appeler la méthode sur le mock
         PokemonMetadata metadata = metadataProvider.getPokemonMetadata(1);
-
         // Vérification des résultats attendus
         assertEquals("Bulbasaur", metadata.getName());
         assertEquals(126, metadata.getAttack());
--- src/test/java/fr/univavignon/pokedex/api/PokemonMetadataTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonMetadataTest.java
@@ -1,52 +1,53 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class PokemonMetadataTest {
 
-    private PokemonMetadata pokemonMetadata;
+    private PokemonMetadata pikachuMetadata;
+    private PokemonMetadata bulbasaurMetadata;
 
     @Before
     public void setUp() {
-        // Initialisation de PokemonMetadata avec des valeurs fictives
-        int index = 1;
-        String name = "Pikachu";
-        int attack = 55;
-        int defense = 40;
-        int stamina = 35;
-
-        pokemonMetadata = new PokemonMetadata(index, name, attack, defense, stamina);
+        // Initialisation de PokemonMetadata avec des valeurs fictives pour plusieurs Pokémon
+        pikachuMetadata = new PokemonMetadata(1, "Pikachu", 55, 40, 35);
+        bulbasaurMetadata = new PokemonMetadata(2, "Bulbasaur", 49, 49, 45);
     }
 
     @Test
     public void testGetIndex() {
         // Test que l'index du Pokémon est correctement initialisé
-        Assert.assertEquals(1, pokemonMetadata.getIndex());
+        Assert.assertEquals(1, pikachuMetadata.getIndex());
+        Assert.assertEquals(2, bulbasaurMetadata.getIndex());
     }
 
     @Test
     public void testGetName() {
         // Test que le nom du Pokémon est correctement initialisé
-        Assert.assertEquals("Pikachu", pokemonMetadata.getName());
+        Assert.assertEquals("Pikachu", pikachuMetadata.getName());
+        Assert.assertEquals("Bulbasaur", bulbasaurMetadata.getName());
     }
 
     @Test
     public void testGetAttack() {
         // Test que l'attaque du Pokémon est correctement initialisée
-        Assert.assertEquals(55, pokemonMetadata.getAttack());
+        Assert.assertEquals(55, pikachuMetadata.getAttack());
+        Assert.assertEquals(49, bulbasaurMetadata.getAttack());
     }
 
     @Test
     public void testGetDefense() {
         // Test que la défense du Pokémon est correctement initialisée
-        Assert.assertEquals(40, pokemonMetadata.getDefense());
+        Assert.assertEquals(40, pikachuMetadata.getDefense());
+        Assert.assertEquals(49, bulbasaurMetadata.getDefense());
     }
 
     @Test
     public void testGetStamina() {
         // Test que l'endurance du Pokémon est correctement initialisée
-        Assert.assertEquals(35, pokemonMetadata.getStamina());
+        Assert.assertEquals(35, pikachuMetadata.getStamina());
+        Assert.assertEquals(45, bulbasaurMetadata.getStamina());
     }
 }
