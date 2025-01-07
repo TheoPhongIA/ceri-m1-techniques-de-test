48460c62d1aa29aa02f279d5dd5ba42af8eff3b9
TheoPhongIA <theophong@outlook.fr>
2025-01-07 23:16:52 +0100

    TP4 Coverage Test 4 sur IPokemonMetadataProviderTest.java

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class PokemonFactoryTest {
+public class IPokemonMetadataProviderTest {
 
     private PokemonFactory pokemonFactory;
     private IPokemonMetadataProvider metadataProvider;
