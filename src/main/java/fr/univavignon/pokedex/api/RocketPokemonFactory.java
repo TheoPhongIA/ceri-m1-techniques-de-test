58e026fdaf43167d7da7fb54ff2d0c2507c5aed0
TheoPhongIA <theophong@outlook.fr>
2025-01-08 09:39:07 +0100

    TP6 Test RocketPokemonFactory

--- src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
@@ -1,11 +1,9 @@
-package fr.univavignon.pokedex.imp;
+package fr.univavignon.pokedex.api;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-import fr.univavignon.pokedex.api.IPokemonFactory;
-import fr.univavignon.pokedex.api.Pokemon;
 import org.apache.commons.collections4.map.UnmodifiableMap;
 
 public class RocketPokemonFactory implements IPokemonFactory {
